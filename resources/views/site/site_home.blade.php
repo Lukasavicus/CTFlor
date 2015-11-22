c825b1358433c2bbb0d205177a63565858efbe88
Marcos Cavalcante <marcos.bcc2011@gmail.com>
2015-11-22 00:41:21 -0200

    Last fix

--- resources/views/site/site_home.blade.php
+++ resources/views/site/site_home.blade.php
@@ -66,54 +66,26 @@
 
 				</div>
 
-
-
-
-
-
-
-   	 			<div class="row col s12">
+ 	 			<div class="row">
 	   	 			<div class="card">
-						<div class="card-image waves-effect waves-block waves-light">
-					    	<img class="activator" src="images/CTFlor1.jpg"  style="width:40%; max-height:100px;" >
-					    </div>
-					    <div class="card-content">
-					    	<span class="card-title activator grey-text text-darken-4">CTFlor - 2014<i class="material-icons right">more_vert</i></span>
-					    	<p><a href="#">Cadastre-se</a></p>
-					    </div>
-					    <div class="card-reveal">
-					    	<span class="card-title grey-text text-darken-4">CTFlor - 2014<i class="material-icons right">close</i></span>
-					    	<p>
-					    		O CTFlor 2014 é um evento que ocorrereu na UFSCar e serve para demontrar a dinâmica do site na visão do cliente final e consumidor  da aplicação.
-					    		<br>
-					    		Outra apresentação retirada do site:
-					    		<br>
-				    			http://www.congressoflorestal.com.br/apresentacao.html
-				    			<br>
-								Desde seu estágio primitivo, o Homem vive em contato íntimo com as florestas. Delas vêm obtendo muitos recursos essenciais para seu sustento, seja modificando, transformando, adequando ou, simplesmente, apropriando-se de sua diversidade de forma extrativista. Além de alimento, as florestas também fornecem ao Homem abrigo, refúgio e bem-estar, de forma direta. Indiretamente, pode-se dizer que os recursos florestais nos provêm em todas as nossas necessidades, condicionando a continuação da vida.
-								<br>
-								Devido ao grande benefício dos recursos florestais, o Homem acabou degradando a maior parte de suas áreas florestadas, sendo que muitas ficaram completamente dizimadas. Apesar do Brasil ainda possuir uma vasta área de florestas naturais, é importante reconhecer que há uma pressão muito grande para conservação desses recursos. Essa pressão aumenta ainda mais, principalmente no âmbito internacional, com o contexto de mudanças climáticas e com o reconhecimento da necessidade de práticas sustentáveis. Além disso, o desenvolvimento econômico, atrelado à expansão da agricultura e dos centros urbanos, vem causando forte pressão nos ecossistemas brasileiros.
-								<br>
-								Assim, as florestas plantadas assumem fundamental importância para suprir, de maneira racional e sustentável, o constante aumento da demanda por produtos madeireiros. O Brasil está na vanguarda em termos de avanços tecnológicos na produção de florestas plantadas, contrapondo com a prática do desmatamento irracional de seus ecossistemas naturais, particularmente o Cerrado.
-								<br>
-								O bioma Cerrado, em especial o seu solo, tem sido o suporte para a produção de grãos, fibras e derivados animais. Apesar do desenvolvimento econômico gerado, a expansão das fronteiras agrícolas para a região do Cerrado vem causando danos a esse bioma tão importante pela sua riqueza de espécies, muitas endêmicas desse tipo de savana.
-								<br>
-								O cultivo de florestas comerciais e de recomposição da natureza em áreas do bioma Cerrado é inadiável e deve ser considerado uma ação ostensiva e decisiva, na tentativa de reverter os passivos ambientais e para fornecer produtos florestais madeireiros e não madeireiros de forma sustentável. O fornecimento de matéria-prima sustentável, a partir de florestas plantadas, é fundamental para evitar que a demanda por esses produtos sejam supridos a partir do extrativismo e da exploração da vegetação nativa.
-								<br>
-								O CONGRESSO FLORESTAL NO CERRADO (CFC) surgiu a partir do Simpósio de Eucaliptocultura em Goiás. Já em sua primeira edição, no ano de 2013, o CFC foi um sucesso. Com palestrantes de renome no cenário técnico-científico florestal, bem como autoridades importantes no cenário das políticas florestais do país, o I CFC atraiu um público de mais de 600 pessoas de todo o Brasil.
-								<br>
-								Com esse sucesso, o Congresso Florestal no Cerrado consolidou a sua pretensão de ser o grande marco para o desenvolvimento florestal nesta região do País. A programação da segunda edição do evento estará enriquecida com a presença de respeitados e importantes profissionais do setor florestal brasileiro que participarão de palestras específicas, mesas redondas e discussões em grupos com a participação de um seleto público interessado nas questões florestais. Dessa maneira o evento pretende catalisar o desenvolvimento da região de Cerrado a partir da integração e compartilhamento de idéias, experiências e vivências resultantes da prática de produtores rurais, empresários e demais profissionais que atuam nos diferentes segmentos do agronegócio florestal, com o conhecimento técnico científico de professores, pesquisadores, engenheiros florestais, agrônomos, biólogos e acadêmicos dessas áreas do conhecimento. Complementarmente, serão expostos e divulgados trabalhos técnico-científicos na forma de “pôsters”.
-								<br>
-								O evento será realizado no período de 10 a 12 de junho de 2015, na cidade de Goiânia, a bela e promissora jovem Capital do Estado de Goiás. A cidade é detentora de um dos melhores índices de qualidade de vida do Brasil e oferece inúmeras oportunidades profissionais, culturais e de lazer.
-								<br>
-								Esta será mais uma grande oportunidade para o Setor Florestal na região do Cerrado, e a Comissão Organizadora se sentirá honrada, pois sua presença e participação serão o grande e desejado diferencial para o êxito do II CONGRESSO FLORESTAL NO CERRADO.
-								<br>
-								Goiânia, 22 de setembro de 2014.
-								Comissão Organizadora.
 
-					    	</p>
-					 	</div>
-					</div>
+								<div class="card-image">
+									  <img src="images/CTFlor1.jpg" style="width:1000px;height:200px;">
+									  <!-- <span class="card-title">CTFlor - 2015</span> -->
+					    	</div>
+
+						    <div class="card-content">
+											<p>
+												A formação do Engenheiro Florestal envolve diversos campos de atuação.
+												Da área de ciências básicas vêm os conhecimentos sobre mecânica, genética,
+												bioquímica entre outros que permitem ao futuro profissional desenvolver tecnologias e
+												produtos aplicados ao meio ambiente e à sua conservação. Para isto, contribuem
+												também os conhecimentos advindos das ciências biológicas com a consolidação das bases
+												necessárias para manejar, conservar e prevenir problemas ambientais, propondo soluções
+												sustentáveis econômica, ecológica e socialmente ajustadas à nova realidade do século 21.
+											</p>
+						    </div>
+					  </div>
 				</div>
 
     	</div>
